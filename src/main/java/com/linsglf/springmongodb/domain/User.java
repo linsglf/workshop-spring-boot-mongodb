e2c38033e35f54733ca299eadafca1f805a276ed
Gabriel Lins <101433324+linsglf@users.noreply.github.com>
2023-12-17 03:01:44 -0300

    Referenciando os posts do usuário

--- src/main/java/com/linsglf/springmongodb/domain/User.java
+++ src/main/java/com/linsglf/springmongodb/domain/User.java
@@ -2,9 +2,12 @@ package com.linsglf.springmongodb.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,4 +22,13 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
