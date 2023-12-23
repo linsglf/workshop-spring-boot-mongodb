fd4ba4e9c93c891f77377b3bcfb8d2ecc1c0153e
Gabriel Lins <101433324+linsglf@users.noreply.github.com>
2023-12-23 01:33:59 -0300

    Consulta com varios criterios

--- src/main/java/com/linsglf/springmongodb/repository/PostRepository.java
+++ src/main/java/com/linsglf/springmongodb/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.linsglf.springmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
@@ -15,4 +16,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     @Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
     List<Post> searchTitle(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
